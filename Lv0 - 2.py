405bdde8cd999baa14b73d75b623efab5e91f3d6
whibenchanter <whibenchanter@gmail.com>
2025-11-13 14:28:57 +0900

    feat: added ~

--- Lv0 - 2.py
+++ Lv0 - 2.py
@@ -1,7 +1,11 @@
 ### TL;DR : 
 
 
+
 # 정수 num1과 num2가 주어질 때, num1에서 num2를 뺀 값을 return하도록 soltuion 함수를 완성해주세요.dd
+
+# 정수 num1과 num2가 주어질 때, num1에서 num2를 뺀 값을 return하도록 soltuion 함수를 완성해주세요.
+
 
 
 
@@ -27,4 +31,8 @@ def solution(num1, num2):
 
 
 # 다른사람이 푼 방식
+
 solution = lambda num1, num2 : num1 - num2
+
+solution = lambda num1, num2 : num1 - num2
+
